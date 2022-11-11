7946e7027a08ee45a3798a24b10532e2bfcc6614
David Vashakidze <83545222+vashaka@users.noreply.github.com>
2022-11-11 13:51:15 +0400

    portscanner is ready

--- portScanner.py
+++ portScanner.py
@@ -1,6 +1,10 @@
 import socket
 import termColor
 
+def scan(target, port):
+    for port in range(1, ports):
+        scan_port(target, port)
+
 def scan_port(ipaddress, port):
     try:
         sock = socket.socket()
@@ -10,5 +14,12 @@ def scan_port(ipaddress, port):
     except:
         print("[-] Port Closed " + str(port))
         
-target = input("[*] Enter Target To Scan: ")
+targets = input("[*] Enter Targets To Scan(split the by ,): ")
 ports = input("[*] Enter How Manu Ports You Want To Scan: ")
+
+if ',' in targets:
+    print("[*] Scannong multiple Targets")
+    for ip_addr in targets.split(','): 
+        scan(ip_addr.strip(' '), ports)
+else:
+    scan(targets, ports)
